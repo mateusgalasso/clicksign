6036e3ef1f19729b78fd19321f8d00c4297ca4da
Mateus Galasso <mateusgalasso@yahoo.com.br>
2020-08-19 21:01:05 -0300

    ajustes na criação do signatário

--- src/Clicksign.php
+++ src/Clicksign.php
@@ -4,7 +4,6 @@ namespace Stonkeep\Clicksign;
 
 use Illuminate\Contracts\Filesystem\FileNotFoundException;
 use Illuminate\Http\Client\Response;
-use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Storage;
 
@@ -26,7 +25,9 @@ class Clicksign
         } catch (\Exception $e) {
             return;
         }
+        //Mount base URL
         $this->urlBase = env('CLICKSIGN_DEV_MODE', true) ? env('CLICKSIGN_DEV_URL', 'https://sandbox.clicksign.com') : env('CLICKSIGN_PROD_URL', 'https://app.clicksign.com');
+        //mount full url
         $this->fullUrl = $this->urlBase . $this->url . $this->accessToken;
     }
 
@@ -41,6 +42,9 @@ class Clicksign
      */
     public function createDocument(String $path, $deadline = null, $autoClose = true, $locale = 'pt-BR', $sequence_enabled = false)
     {
+        //Verify if parameters were passed
+        throw_if(!isset($path), 'Some parameters are unset');
+        //Mount body
         $body = [
             "document" => [
                 "path" => "/$path",
@@ -52,5 +56,36 @@ class Clicksign
             ]
         ];
         return Http::post("$this->urlBase/api/v1/documents?access_token=$this->accessToken", $body);
+    }
+
+    /**
+     * @param String $email
+     * @param String $name
+     * @param null $phoneNumber
+     * @param bool $documentation
+     * @param null $birthday
+     * @param bool $has_documentation
+     * @return Response
+     * @throws \Throwable
+     */
+    public function createSigner(String $email, String $name, $phoneNumber = null, $documentation = false, $birthday = null, $has_documentation = false)
+    {
+        //Verify if parameters were passed
+        throw_if(!isset($email) or !isset($name), 'Some parameters are unset');
+        //Mount body
+        $body = [
+            "signer" => [
+                "email" => $email,
+                "phone_number" => $phoneNumber,
+                "auths" => [
+                    "email"
+                ],
+                "name" => $name,
+                "documentation" => $documentation,
+                "birthday" => $birthday,
+                "has_documentation" => $has_documentation
+            ]
+        ];
+        return Http::post("$this->urlBase/api/v1/signers?access_token=$this->accessToken", $body);
     }
 }
